f0c9179a6b79dd343d05b1a8934645c8792241fb
stefanRitter <stefan@stefanritter.com>
2013-04-18 08:39:58 +0100

    switch from android's FloatMath to java.lang.Math

--- src/com/twopointfived/engine/SpatialHashGrid.java
+++ src/com/twopointfived/engine/SpatialHashGrid.java
@@ -18,8 +18,7 @@ package com.twopointfived.engine;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import android.util.FloatMath;
+import java.lang.Math;
 
 public class SpatialHashGrid {
     List<GameObject>[] dynamicCells;
@@ -33,8 +32,8 @@ public class SpatialHashGrid {
     @SuppressWarnings("unchecked")
     public SpatialHashGrid(float worldWidth, float worldHeight, float cellSize) {
         this.cellSize = cellSize;
-        this.cellsPerRow = (int)FloatMath.ceil(worldWidth/cellSize);
-        this.cellsPerCol = (int)FloatMath.ceil(worldHeight/cellSize);
+        this.cellsPerRow = (int)Math.ceil(worldWidth/cellSize);
+        this.cellsPerCol = (int)Math.ceil(worldHeight/cellSize);
         int numCells = cellsPerRow * cellsPerCol;
         dynamicCells = new List[numCells];
         staticCells = new List[numCells];
@@ -104,10 +103,10 @@ public class SpatialHashGrid {
     }
 
     public int[] getCellIds(GameObject obj) {
-        int x1 = (int)FloatMath.floor(obj.bounds.lowerLeft.x / cellSize);
-        int y1 = (int)FloatMath.floor(obj.bounds.lowerLeft.y / cellSize);
-        int x2 = (int)FloatMath.floor((obj.bounds.lowerLeft.x + obj.bounds.width) / cellSize);
-        int y2 = (int)FloatMath.floor((obj.bounds.lowerLeft.y + obj.bounds.height) / cellSize);
+        int x1 = (int)Math.floor(obj.bounds.lowerLeft.x / cellSize);
+        int y1 = (int)Math.floor(obj.bounds.lowerLeft.y / cellSize);
+        int x2 = (int)Math.floor((obj.bounds.lowerLeft.x + obj.bounds.width) / cellSize);
+        int y2 = (int)Math.floor((obj.bounds.lowerLeft.y + obj.bounds.height) / cellSize);
         
         if(x1 == x2 && y1 == y2) {
             if(x1 >= 0 && x1 < cellsPerRow && y1 >= 0 && y1 < cellsPerCol)
